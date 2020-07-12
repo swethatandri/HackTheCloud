40b9a824af322af570604dbde36d64c5c9a4bd47
aleesfa <68164484+aleesfa@users.noreply.github.com>
2020-07-12 13:58:23 -0700

    Update Servlet2.java

--- Servlet2.java
+++ Servlet2.java
@@ -2,21 +2,20 @@ import java.sql.*;
 
 //This Class inputs user information into SPL Database
 //Need to replace the link in parentheses to correspond to the HTTP page
-@WebServlet("/partners.php")
-public class Servlet2 extends HttpServlet 
+@WebServlet("/MentalHealth/*")
+public class Servlet2 extends javax.servlet.http.HttpServlet 
 {
     protected void doPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
-        //replace the getParameter variables
         String category = request.getParameter("address");
         String statement= request.getParameter("category");
         try
         {
             // Ask or find out the database url
-            String url = "";
+            String url = "/MentalHealth/*";
             //In a previous program, I replace url with "local host" but another tutorial says to use url. 
             //It is preferable to use url IF WE CAN FIND IT
-            Connection conn = DriveManager.getConnection(url, "root", "");
+            Connection conn = DriveManager.getConnection("/MentalHealth/*", "root", "");
             Statement stat = conn.createStatement();
             stat.executeUpdate("INSERT INTO MedicalTable " + "VALUES (category, statement)");
             conn.close();
