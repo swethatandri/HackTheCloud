9bae0bea25a005ac74c1dbfa7c831ea10377b7d1
aleesfa <68164484+aleesfa@users.noreply.github.com>
2020-07-12 19:07:15 -0700

    Update Servlet2.java

--- Servlet2.java
+++ Servlet2.java
@@ -1,9 +1,16 @@
 import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calender;
+ 
 
 //This Class inputs user information into SPL Database
 //Need to replace the link in parentheses to correspond to the HTTP page
-@WebServlet("/MentalHealth/*")
-public class Servlet2 extends javax.servlet.http.HttpServlet 
+@WebServlet("update")
+public class Servlet2 extends HttpServlet 
 {
     protected void doPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
@@ -12,10 +19,10 @@ public class Servlet2 extends javax.servlet.http.HttpServlet
         try
         {
             // Ask or find out the database url
-            String url = "/MentalHealth/*";
+            String url = "update";
             //In a previous program, I replace url with "local host" but another tutorial says to use url. 
             //It is preferable to use url IF WE CAN FIND IT
-            Connection conn = DriveManager.getConnection("/MentalHealth/*", "root", "");
+            Connection conn = DriveManager.getConnection("update", "root", "");
             Statement stat = conn.createStatement();
             stat.executeUpdate("INSERT INTO MedicalTable " + "VALUES (category, statement)");
             conn.close();
