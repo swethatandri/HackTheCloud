2e9e62414d5dd850f29de9d4f58a4ecad60d768a
aleesfa <68164484+aleesfa@users.noreply.github.com>
2020-07-12 19:42:31 -0700

    Update Partners.java

--- Partners.java
+++ Partners.java
@@ -1,4 +1,8 @@
 //Curently this is just for the page where user inputs email and category
+import java.io.*;
+import java.sql.SQLException;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
