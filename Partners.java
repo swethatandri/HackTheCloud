5d3755054e8c5e55d02ceb3a6ca162f1a1c5b4ea
aleesfa <68164484+aleesfa@users.noreply.github.com>
2020-07-12 19:08:30 -0700

    Update Partners.java

--- Partners.java
+++ Partners.java
@@ -1,6 +1,12 @@
 //Curently this is just for the page where user inputs email and category
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
 @WebServlet("/category")
-public class Partners extends HttpServlet 
+public class Servlet extends HttpServlet 
 {
     protected void doPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
